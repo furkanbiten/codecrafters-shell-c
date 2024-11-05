0384235cfc620c482aac503771dceac47f919a1f
Ali Furkan Biten <furkanbiten@gmail.com>
2024-11-05 03:08:01 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -11,6 +11,10 @@ int main()
         char input[100];
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
+        if (strncmp(input, "exit 0", 100) == 0){
+            break;
+        }
+
         printf("%s: command not found\n", input);
     }
     return 0;
