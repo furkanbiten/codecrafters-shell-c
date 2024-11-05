0cf725104c7d5b900daa1f7540d40cfeff015116
Ali Furkan Biten <furkanbiten@gmail.com>
2024-11-05 03:27:36 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -12,14 +12,23 @@ int main()
         char input[100];
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
-        if (!strncmp(input, "exit 0", 100)) {
+        if (!strncmp(input, "exit ", 5)) {
             exit(0);
-        }
-        if (!strncmp(input, "echo", 4)) {
-            for (int i=5; i<strlen(input); i++){
-                printf("%c", input[i]);
+        } else if (!strncmp(input, "type ", 5)) {
+            if (!strncmp(input + 5, "echo", 4)) {
+                printf("echo is a shell builtin\n");
+            } else if (!strncmp(input + 5, "exit", 4)) {
+                printf("exit is a shell builtin\n");
+            } else if (!strncmp(input + 5, "cat", 4)) {
+                printf("cat is /bin/cat\n");
+            } else if (!strncmp(input + 5, "type", 4)) {
+                printf("type is a shell builtin\n");
+            } else {
+                printf("%s: not found\n", input+5);
             }
-            printf("\n");
+
+        } else if (!strncmp(input, "echo ", 5)) {
+            printf("%s\n", input + 5);
         } else {
             printf("%s: command not found\n", input);
         }
