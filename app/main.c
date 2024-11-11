ad3fc397385478a7e2c1f1b063b158f0c07bd1da
Ali Furkan Biten <furkanbiten@gmail.com>
2024-11-11 22:37:07 +0100

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -36,6 +36,38 @@ char* find_in_path(char* command)
     return NULL;
 }
 
+void type_command(char* input)
+{
+    char* cmd = input + 5;
+
+    if (strncmp(cmd, "echo", 4) == 0 || strncmp(cmd, "pwd", 3) == 0 || strncmp(cmd, "exit", 4) == 0 || strncmp(cmd, "type", 4) == 0) {
+        printf("%s is a shell builtin\n", cmd);
+    } else {
+        char* cmd_path = find_in_path(cmd);
+        if (cmd_path) {
+            printf("%s is %s\n", cmd, cmd_path);
+        } else {
+            printf("%s: not found\n", cmd);
+        }
+    }
+}
+
+bool check_others(char* input){
+    char* input_copy = strdup(input);
+    char* cmd = strtok(input_copy, " ");
+    char* args = strtok(NULL, "");
+    char* cmd_path = find_in_path(cmd);
+    if (cmd_path) {
+        char temp_buffer[1024];
+        sprintf(temp_buffer, "%s %s", cmd_path, args);
+
+        system(temp_buffer);
+        free(input_copy);
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -60,32 +92,18 @@ int main(int argc, char* argv[])
             continue;
         }
 
-        if (strncmp(input, "type ", 5) == 0) {
-            char* cmd = input + 5;
-
-            if (strncmp(cmd, "echo", 4) == 0 || strncmp(cmd, "exit", 4) == 0 || strncmp(cmd, "type", 4) == 0) {
-                printf("%s is a shell builtin\n", cmd);
-            } else {
-                char* cmd_path = find_in_path(cmd);
-                if (cmd_path) {
-                    printf("%s is %s\n", cmd, cmd_path);
-                } else {
-                    printf("%s: not found\n", cmd);
-                }
-            }
+        if (strncmp(input, "pwd", 3) == 0) {
+            system("pwd");
             continue;
         }
 
-        char* input_copy = strdup(input);
-        char* cmd = strtok(input_copy, " ");
-        char* args = strtok(NULL, "");
-        char* cmd_path = find_in_path(cmd);
-        if (cmd_path) {
-            char temp_buffer[1024];
-            sprintf(temp_buffer, "%s %s", cmd_path, args);
+        if (strncmp(input, "type ", 5) == 0) {
+            type_command(input);
+            continue;
+        }
 
-            system(temp_buffer);
-            free(input_copy);
+        bool found_cmd = check_others(input);
+        if (found_cmd){
             continue;
         }
 
